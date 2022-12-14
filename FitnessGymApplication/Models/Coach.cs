7f17887e354fa87ee7ff2768426e1a217989a36a
gldra <chaiebrania1@gmail.com>
2022-12-14 12:02:11 +0100

    change namespaceCoach

--- FitnessGymApplication/Models/Coach.cs
+++ FitnessGymApplication/Models/Coach.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
 
-namespace GYM_FITNESS.Models
+namespace FitnessGymApplication.Models
 {
     public class Coach
     {
