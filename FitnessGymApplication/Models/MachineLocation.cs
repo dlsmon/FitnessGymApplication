99cc16691da663751cadf171a54c5a8e3d1df4f3
dlsmon <118259688+dlsmon@users.noreply.github.com>
2022-12-15 10:23:52 +0100

    ManyToMany Test
    For Rania

---
+++ FitnessGymApplication/Models/MachineLocation.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitnessGymApplication.Models
+{
+    public class MachineLocation
+    {
+
+        [Key]
+        public int ID { get; set; }
+
+        //Foreign KEYS
+        [ForeignKey("IdMachine")]
+        public virtual Machine? Machine { get; set; }
+
+        [ForeignKey("IdLocation")]
+        public virtual Location? Location { get; set; }
+
+
+    }
+}
