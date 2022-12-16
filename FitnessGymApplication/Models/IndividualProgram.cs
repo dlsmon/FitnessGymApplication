932da483f21a0c8f4730705d58f9e83a1beae823
dlsmon <118259688+dlsmon@users.noreply.github.com>
2022-12-16 14:27:34 +0100

    model correction

--- FitnessGymApplication/Models/IndividualProgram.cs
+++ FitnessGymApplication/Models/IndividualProgram.cs
@@ -5,6 +5,9 @@ namespace FitnessGymApplication.Models
 {
     public class IndividualProgram
     {
+        [Key]
+        public int Id { get; set; }
+
         //Training program as a collective will have
         public int IdTrainingProgram { get; set; }
         public int IdExercise { get; set; }
