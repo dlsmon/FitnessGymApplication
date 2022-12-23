34c1e9f13ec0e7912ba959eb3148a40741be93e3
Daikav <118259434+Daikav@users.noreply.github.com>
2022-12-23 10:28:30 +0100

    Seed exercise test

--- FitnessGymApplication/Data/Seeding/AppDBContextSeed.cs
+++ FitnessGymApplication/Data/Seeding/AppDBContextSeed.cs
@@ -1,6 +1,39 @@
-﻿namespace FitnessGymApplication.Data.Seeding
+﻿using FitnessGymApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessGymApplication.Data.Seeding
 {
     public class AppDBContextSeed
     {
+        public static void Seed(ModelBuilder modelBuilder)
+        {
+            //Exercise 
+
+            modelBuilder.Entity<Exercise>().HasData(
+                new Exercise()
+                {
+                    Id = 1,
+                    Name = "Crunch",
+                    Description = "Description",
+                    Duration = 15,
+                    Calories = 100
+                });
+
+            //Individual Program               
+
+            //Training program               
+
+            //Formula
+
+            //Coach 
+
+            //Speciality
+
+            //Location
+
+            //Machine
+
+            //Product
+        }
     }
 }
