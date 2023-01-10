f9e8c88f2304dd0669411a5d91db2cb2cb7225a4
Daikav <118259434+Daikav@users.noreply.github.com>
2023-01-10 17:42:39 +0100

    add my account & register view and controller

---
+++ FitnessGymApplication/Controllers/MyAccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessGymApplication.Controllers
+{
+    public class MyAccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }      
+
+        public IActionResult MyInformation()
+        {
+            return View();
+        }
+
+        public IActionResult MyGoal()
+        {
+            return View();
+        }
+
+        public IActionResult MyReservation()
+        {
+            return View();
+        }
+
+     
+    }
+}
---
+++ FitnessGymApplication/Controllers/RegisterController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessGymApplication.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()  //Register's index is the Sign UP
+        {
+            return View();
+        }
+
+        public IActionResult MyQuiz()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
