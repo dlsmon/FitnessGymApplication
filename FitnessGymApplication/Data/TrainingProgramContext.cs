cde9e9a67884234595d9a09f658e56820cb3e219
dlsmon <118259688+dlsmon@users.noreply.github.com>
2023-01-13 09:48:05 +0100

    context testing

---
+++ FitnessGymApplication/Data/TrainingProgramContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FitnessGymApplication.Models;  
+using FitnessGymApplication.Data.Seeding;
+
+namespace FitnessGymApplication.Data
+{
+    public class TrainingProgramContext : DbContext
+    {
+        public TrainingProgramContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+
+        }
+
+        //Seeding Database
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            AppDBContextSeed.Seed(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<FitnessGymApplication.Models.Exercise> Exercise { get; set; } = default!;
+        public DbSet<FitnessGymApplication.Models.TrainingProgram> TrainingProgram { get; set; } = default!;
+        public DbSet<FitnessGymApplication.Models.IndividualProgram> IndividualProgram { get; set; } = default!;
+        public DbSet<FitnessGymApplication.Models.Session> Session { get; set; } = default!;
+
+    }
+}
